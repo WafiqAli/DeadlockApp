14f00ffa4060ee11512bc52b54c53dfda6ccae3f
WafiqAli <wafiqcali@gmail.com>
2026-07-22 02:35:04 +1200

    Fixed some files not being in the correct folder/namespace

--- api/Clients/IDeadlockApiClient.cs
+++ api/Clients/IDeadlockApiClient.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Clients
+namespace api.Clients
 {
     public interface IDeadlockApiClient
     {
-        
+
     }
 }
--- api/DTOs/HeroDTO.cs
+++ api/DTOs/HeroDTO.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DTOs
+namespace api.DTOs
 {
     public class HeroDTO
     {
-        
+
     }
 }
