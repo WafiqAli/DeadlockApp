82755fe39b9e3f81ead97660f5026145e73bb08a
WafiqAli <wafiqcali@gmail.com>
2026-07-22 16:42:43 +1200

    Added heroservice and httpclient to builder in program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,20 @@
+using api.Clients;
+using api.Services;
+using api.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IHeroesService, HeroesService>();
+
+builder.Services.AddHttpClient<IDeadlockApiClient, DeadlockApiClient>(
+    httpClient =>
+    {
+        httpClient.BaseAddress = new Uri("https://api.deadlock-api.com/");
+    });
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
