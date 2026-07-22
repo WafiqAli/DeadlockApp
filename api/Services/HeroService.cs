8027d69c0678ba91444f23b57ee0a89ddcda1c50
WafiqAli <wafiqcali@gmail.com>
2026-07-22 16:05:22 +1200

    Added logic to HeroService its interface

--- api/Services/HeroService.cs
+++ api/Services/HeroService.cs
@@ -2,12 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Clients;
+using api.DTOs;
 using api.Services.Interfaces;
 
 namespace api.Services
 {
     public class HeroService : IHeroService
     {
+        private readonly IDeadlockApiClient _deadlockApiClient;
 
+        public HeroService(IDeadlockApiClient deadlockApiClient)
+        {
+            _deadlockApiClient = deadlockApiClient;
+        }
+
+        public async Task<IReadOnlyList<HeroDto>> GetHeroesAsync(CancellationToken cancellationToken = default)
+        {
+            var heroes = await _deadlockApiClient.GetHeroesAsync(cancellationToken);
+
+            return heroes
+                .OrderBy(hero => hero.Name)
+                .ToList();
+        }
     }
 }
--- api/Services/IHeroService.cs
+++ api/Services/IHeroService.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.DTOs;
 
 namespace api.Services.Interfaces
 {
     public interface IHeroService
     {
-        
+        /* IReadOnlyList gives an ordered collection to read but not one that should be modified. 
+           This service should pass back the data it retrived from the client to the controller without needing to change anything. 
+        */
+        Task<IReadOnlyList<HeroDto>> GetHeroesAsync(CancellationToken cancellationToken = default);
     }
 }
