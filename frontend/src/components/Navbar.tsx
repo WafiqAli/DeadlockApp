2783365d5f4c2af741a5d9398a34d436f81faea1
WafiqAli <wafiqcali@gmail.com>
2026-08-07 16:46:47 +1200

    navbar update: adding tailwind CSS. Added CSS themes in index.css

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,18 +2,26 @@ import { Sun } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>Deadlock</h1>
-      <ul>
-        <li>Home</li>
-        <li>Heroes</li>
-        <li>Items</li>
-        <input type="text" placeholder="Search player..." />
-        <button>
+    <nav className="flex items-center justify-between border border-border h-20 px-8 bg-navbar font-medium">
+      <div className="flex gap-8">
+        <h1 className="text-2xl font-medium">DEADLOCK.GG</h1>
+        <ul className="flex gap-8 text-common-text items-center">
+          <li>Home</li>
+          <li>Heroes</li>
+          <li>Items</li>
+        </ul>
+      </div>
+      <div className="flex gap-8 text-common-text">
+        <input
+          type="text"
+          placeholder="Search player..."
+          className="input-light p-1.5"
+        />
+        <button className="button-light px-2">
           <Sun />
         </button>
-        <button>Sign in</button>
-      </ul>
+        <button className="button-light px-5 py-2">Sign in</button>
+      </div>
     </nav>
   );
 };
